0973af2056bb41f32d3415aab30143a46787a7f2
Faisal <faisal.shaik.82174@gmail.com>
2024-12-05 12:36:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-
+document.querySelectorAll('.profile').forEach(profile => {
+    profile.addEventListener('mouseenter', () => {
+      profile.style.backgroundColor = '#f0f0f0';
+    });
+    profile.addEventListener('mouseleave', () => {
+      profile.style.backgroundColor = 'white';
+    });
+  });
